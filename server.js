8b35507802c36061215cf0865cdd5cdb5dc9c6d2
Molly Kent <kent.molly@gmail.com>
2015-06-11 20:37:04 -0700

    made funtioning server and set up endpoints in server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+var Adjective = function() {
+  this.goofy = true;
+  this.fancy = true;
+  this.cantankerous = true;
+  this.fluffy = true;
+  this.gilded = true;
+}
+var adjective = new Adjective();
+
+function getRandomWord(object) {
+  var propArray = Object.keys(object);
+  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+  return {word: randomProp};
+}
+
+/*FROM F2*/
+app.get('/', function(req, res) {
+  res.send('Hello World!  You made it to my wimpy webpage.');
+});
+
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
+});
+
+app.listen(port, function() {
+  console.log('Server is running on port ' + port + '.  Woot!');
+});
+
+/*FROM BOOTCAMP*/
+// app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/app'));
+
+// app.get('/', function(req, res) {
+//   res.send('Hello World!  You made it to my wimpy webpage.');
+// });
+
+// app.get('adjective', function(req, res) {
+//   res.json();
+// })
+
+// app.get('/*', function(req, res) {
+//   res.status(404).sendFile(__dirname + '/app/404.html');
+// });
+
+// app.listen(app.get('port'), function(){
+//   console.log("Node app is running at localhost: " + app.get('port'));
+// });
