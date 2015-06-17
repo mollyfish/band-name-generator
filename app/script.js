66b05d3887ed8a0ad7c6209534333d9e95b01cc5
Molly Kent <kent.molly@gmail.com>
2015-06-17 16:37:54 -0700

    trying revised get routes

--- app/script.js
+++ app/script.js
@@ -9,17 +9,15 @@ function capitalize(string) {
 }
 
   $('#pick-string').click( function() {
-    $.get("https://localhost:3000/adjective", function(response) {
+    $.get("/adjective", function(response) {
       var adjResponse = response.word;
       $('#adjective').text(adjResponse);
     })
-
-    $.get("https://localhost:3000/verb", function(response) {
+    $.get("/verb", function(response) {
       var verbResponse = response.word;
       $('#verb').text(verbResponse);
     })
-
-    $.get("https://localhost:3000/noun", function(response) {
+    $.get("/noun", function(response) {
       var nounResponse = response.word;
       $('#noun').text(nounResponse);
       if (nounResponse == "Chairs") {
@@ -38,39 +36,37 @@ function capitalize(string) {
     })
   })
 
-    $("#submitWords").on("submit", function(e) {
-        e.preventDefault();
-        
-        var adjective = capitalize($("[name=adjective]").val());
-        var adjPost;
-        var verb = capitalize($("[name=verb]").val());
-        var verbPost;
-        var noun = capitalize($("[name=noun]").val());
-        var nounPost;
-
-        if (adjective) {
-          adjPost = {word: adjective};
-          $.post("adjective", adjPost, function(response) {
-            var adjectiveRes = response.msg;
-            $("#adjRes").text(adjectiveRes);
-          })
-        }
-        if (verb) {
-          verbPost = {word: verb};
-          $.post("verb", verbPost, function(response) {
-            var verbRes = response.msg;
-            $("#verbRes").text(verbRes);
-          })
-        }
-        if (noun) {
-          nounPost = {word: noun};
-          $.post("noun", nounPost, function(response) {
-            var nounRes = response.msg;
-            $("#nounRes").text(nounRes);
-          })
-        }
-        $("[name=adjective]").val("");
-        $("[name=verb]").val("");
-        $("[name=noun]").val("");
-    });
+  $("#submitWords").on("submit", function(e) {
+    e.preventDefault();
+    var adjective = capitalize($("[name=adjective]").val());
+    var adjPost;
+    var verb = capitalize($("[name=verb]").val());
+    var verbPost;
+    var noun = capitalize($("[name=noun]").val());
+    var nounPost;
+      if (adjective) {
+        adjPost = {word: adjective};
+        $.post("adjective", adjPost, function(response) {
+          var adjectiveRes = response.msg;
+          $("#adjRes").text(adjectiveRes);
+        })
+      }
+      if (verb) {
+        verbPost = {word: verb};
+        $.post("verb", verbPost, function(response) {
+          var verbRes = response.msg;
+          $("#verbRes").text(verbRes);
+        })
+      }
+      if (noun) {
+        nounPost = {word: noun};
+        $.post("noun", nounPost, function(response) {
+          var nounRes = response.msg;
+          $("#nounRes").text(nounRes);
+        })
+      }
+    $("[name=adjective]").val("");
+    $("[name=verb]").val("");
+    $("[name=noun]").val("");
+  });
 });
